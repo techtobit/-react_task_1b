cecea05423feaf17f9b47f5d765a2590134e4a07
techtobit <nurthern2016@gmail.com>
2024-04-27 00:11:25 +0600

    initial setup

---
+++ src/authContext.jsx
@@ -0,0 +1,80 @@
+import React, { useReducer } from "react";
+import MkdSDK from "./utils/MkdSDK";
+
+export const AuthContext = React.createContext();
+
+const initialState = {
+  isAuthenticated: false,
+  user: null,
+  token: null,
+  role: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      if (action.payload.success) {
+        return {
+          ...state,
+          isAuthenticated: true,
+          user: null, // Assuming user details are retrieved separately
+          token: localStorage.getItem("token"),
+          role: localStorage.getItem("role"),
+        };
+      } else {
+        // Handle login failure (e.g., display error message)
+        return state;
+      }
+      break;
+    
+    case "LOGOUT":
+      localStorage.clear();
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+      };
+    default:
+      return state;
+  }
+};
+
+let sdk = new MkdSDK();
+
+export const tokenExpireError = (dispatch, errorMessage) => {
+  const role = localStorage.getItem("role");
+  if (errorMessage === "TOKEN_EXPIRED") {
+    dispatch({
+      type: "Logout",
+    });
+    window.location.href = "/" + role + "/login";
+  }
+};
+
+const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  React.useEffect(() => {
+    const token = localStorage.getItem("token");
+    const role = localStorage.getItem("role");
+
+    if (token && role) {
+      dispatch({
+        type: "LOGIN",
+        payload: { success: true }, // Assuming token presence indicates successful login
+      });
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        state,
+        dispatch,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
---
+++ src/utils/MkdSDK.jsx
@@ -0,0 +1,123 @@
+export default function MkdSDK() {
+  this._baseurl = "https://reacttask.mkdlabs.com";
+  this._project_id = "reacttask";
+  this._secret = "cmVhY3R0YXNrOmQ5aGVkeWN5djZwN3p3OHhpMzR0OWJtdHNqc2lneTV0Nw==";
+  this._table = "";
+  this._custom = "";
+  this._method = "";
+
+  // const raw = this._project_id + ":" + this._secret;
+  const raw = this._secret;
+  let base64Encode = btoa(raw);
+
+  this.setTable = function (table) {
+    this._table = table;
+  };
+  
+  this.login = async function (email, password, role) {
+    const payload = {
+      email,
+      password,
+      role, // Assuming role is passed as a prop during login
+    };
+  
+    const header = this.getHeader();
+  
+    try {
+      const response = await fetch(this.baseUrl() + "/v1/api/rest/auth/LOGIN", {
+        method: "POST",
+        headers: header,
+        body: JSON.stringify(payload),
+      });
+  
+      const jsonResponse = await response.json();
+  
+      if (response.ok) {
+        localStorage.setItem("token", jsonResponse.token);
+        localStorage.setItem("role", role); // Assuming successful login stores role
+        return true;
+      } else {
+        throw new Error(jsonResponse.message);
+      }
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+  
+  
+
+  this.getHeader = function () {
+    return {
+      Authorization: "Bearer " + localStorage.getItem("token"),
+      "x-project": base64Encode,
+    };
+  };
+
+  this.baseUrl = function () {
+    return this._baseurl;
+  };
+  
+  this.callRestAPI = async function (payload, method) {
+    const header = {
+      "Content-Type": "application/json",
+      "x-project": base64Encode,
+      Authorization: "Bearer " + localStorage.getItem("token"),
+    };
+
+    switch (method) {
+      case "GET":
+        const getResult = await fetch(
+          this._baseurl + `/v1/api/rest/${this._table}/GET`,
+          {
+            method: "post",
+            headers: header,
+            body: JSON.stringify(payload),
+          }
+        );
+        const jsonGet = await getResult.json();
+
+        if (getResult.status === 401) {
+          throw new Error(jsonGet.message);
+        }
+
+        if (getResult.status === 403) {
+          throw new Error(jsonGet.message);
+        }
+        return jsonGet;
+      
+      case "PAGINATE":
+        if (!payload.page) {
+          payload.page = 1;
+        }
+        if (!payload.limit) {
+          payload.limit = 10;
+        }
+        const paginateResult = await fetch(
+          this._baseurl + `/v1/api/rest/${this._table}/${method}`,
+          {
+            method: "post",
+            headers: header,
+            body: JSON.stringify(payload),
+          }
+        );
+        const jsonPaginate = await paginateResult.json();
+
+        if (paginateResult.status === 401) {
+          throw new Error(jsonPaginate.message);
+        }
+
+        if (paginateResult.status === 403) {
+          throw new Error(jsonPaginate.message);
+        }
+        return jsonPaginate;
+      default:
+        break;
+    }
+  };  
+
+  this.check = async function (role) {
+    //TODO
+  };
+
+  return this;
+}
