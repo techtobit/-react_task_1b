80fa5421a6f35557fdad67122ecba41f880a137a
techtobit <nurthern2016@gmail.com>
2024-04-27 23:04:51 +0600

    updated border color

--- src/pages/DnD/DraggableItem.jsx
+++ src/pages/DnD/DraggableItem.jsx
@@ -31,14 +31,14 @@ const DraggableItem = ({ item, index, moveItem }) => {
         <div
             ref={(node) => drag(drop(node))}
             style={{ opacity: isDragging ? 0.5 : 1 }}
-            className='flex px-5 justify-between items-center font-thin h-[96px] gap-5 mb-5 border-[1px] rounded-[25px] border-slate-400'
+            className='flex px-5 justify-between items-center font-thin h-[95px] gap-5 mb-5 border-[1px] rounded-[25px] border-slate-700'
         >
             <div className='flex items-center justify-between gap-5'>
                 <div className='px-5'>{item.id}</div>
-                <img className='w-[118px] h-[64px] rounded-md' src={item.photo} />
-                <div className='w-[364px] h-[56px] text-[20px] gap-5 leading-[28px]'>{item.title}</div>
+                <img  loading="lazy" className='w-[118px] h-[64px] rounded-md' src={item.photo} />
+                <div className='w-[364px] h-[56px] flex items-center text-[20px] gap-5 leading-[28px]'>{item.title}</div>
                 <div className='flex item-center pl-10 text-[#DBFD51] gap-3 justfiy-start'>
-                    <img src={avatarIcon} />
+                    <img  src={avatarIcon} />
                     {item.username}
                 </div>
             </div>
